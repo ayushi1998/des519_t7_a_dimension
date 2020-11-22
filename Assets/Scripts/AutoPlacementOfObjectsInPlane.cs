8a2aea5a008644c8561eb927ca7c448aa49e0f5c
Ayushi Srivastava <ayushisri98@gmail.com>
2020-11-22 23:27:47 +0530

    Assets with complete code and materials

---
+++ Assets/Scripts/AutoPlacementOfObjectsInPlane.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARPlaneManager))]
+public class AutoPlacementOfObjectsInPlane : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+    //[SerializeField]
+    //private GameObject welcomePanel;
+
+    [SerializeField]
+    private GameObject placedPrefab_woodPlank;
+
+    private GameObject placedObject_woodPlank;
+
+    [SerializeField]
+    private ARPlaneManager arPlaneManager;
+
+    private void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        //dismissButton.onClick.AddListener(Dismiss);
+        arPlaneManager = GetComponent<ARPlaneManager>();
+        arPlaneManager.planesChanged += PlaneChanged;
+    }
+
+    private void PlaneChanged(ARPlanesChangedEventArgs args)
+    {
+        if (args.added != null && placedObject_woodPlank == null)
+        {
+            ARPlane arPlane = args.added[0];
+            placedObject_woodPlank = Instantiate(placedPrefab_woodPlank, arPlane.transform.position, arPlane.transform.rotation);
+
+        }
+    }
+}
---
+++ Assets/Scripts/DismissCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine;
+using UnityEngine.UI;
+public class DismissCanvas : MonoBehaviour
+{
+
+    public GameObject welcomePanel;
+    public Button dismissButton;
+    public GameObject secondPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+
+    private void Dismiss(){
+        welcomePanel.SetActive(false);
+        secondPanel.SetActive(true);
+    } 
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
